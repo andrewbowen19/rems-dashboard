30d20cefad6a7ab3852d49a62c6c0371cd5a5a85
Andrew Bowen <atbowen@gmail.com>
2024-06-17 21:06:07 -0400

    Add basic time series forecasting tabbed layout (#7)
    * Add dose chart
    
    * clean up dashboard
    
    * add tidyr
    
    * revert file path to GH
    
    * Update TED title
    
    * Update README
    
    * adding tabbed layout
    
    * Add in basic TS
    
    * Add TS
    
    * add ARIMA forecast to TS tab
    
    * Add basic X and Y Histograms

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(readxl)
 library(dplyr)
@@ -15,8 +6,11 @@ library(ggplot2)
 library(bslib)
 library(ggExtra)
 library(glue)
+library(tsibble)
+library(fabletools)
+library(fable)
 
-# Read in dataset: return to Git link when pushed!!
+# Read in dataset
 df <- read.csv("https://raw.githubusercontent.com/andrewbowen19/rems-dashboard/main/rems-data.csv",
                check.names=FALSE)
 
@@ -29,57 +23,71 @@ df_num <- df %>% select(where(is.numeric), -c(`Program Office`,
                                               `Labor Category`,
                                               `Occupation`,
                                               `Monitoring Status`))
+                                
 
-# Set up sidebar
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Radiation Exposure Monitoring System Dashboard", windowTitle = "REMS Dashboard"),
-  
-  sidebarLayout(
-    
-    # Sidebar with a slider input
-    sidebarPanel(
-      selectInput("site",
-                  "Site", unique(df$Site), "Savannah River Site", multiple=TRUE),
-      varSelectInput("xvar", "X variable", df_num, selected = "Total Number Monitored"),
-      varSelectInput("yvar", "Y variable", df_num, selected = "Average Meas. TED (mrem)"),
-      selectInput(
-        "program_office", "Filter by Program Office",
-        choices = unique(df$`Program Office`),
-        selected = "National Nuclear Security Administration",
-        multiple=TRUE
-      ),
-      sliderInput("year", "Year Range", value = c(1986, 2022), min=1986, max=2022),
-      checkboxInput("show_site", "Show Site", TRUE),
-      checkboxInput("show_margins", "Show marginal plots", TRUE),
-      tags$a(href="https://orise.orau.gov/cer/rems/definitions.pdf", "Definitions")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tags$a(href="https://www.energy.gov/ehss/occupational-radiation-exposure-rems-system-tools",
-             "All data sourced from the Department of Energy REMS Query Tool"),
-      plotOutput("scatter"),
-      hr(),
-      plotOutput("timeSeries"),
-      hr(),
-      plotOutput("tedGraph"), 
-      
-    )
+# Sidebar content
+sidebar_content <- sidebarPanel(
+  selectInput("site", "Site", unique(df$Site), "Savannah River Site", multiple=TRUE),
+  varSelectInput("xvar", "X variable", df_num, selected = "Total Number Monitored"),
+  varSelectInput("yvar", "Y variable", df_num, selected = "Average Meas. TED (mrem)"),
+  selectInput("program_office", "Filter by Program Office", choices = unique(df$`Program Office`), selected = "National Nuclear Security Administration", multiple=TRUE),
+  sliderInput("year", "Year Range", value = c(1986, 2022), min=1986, max=2022),
+  checkboxInput("show_site", "Show Site", TRUE),
+  checkboxInput("show_margins", "Show marginal plots", TRUE),
+  tags$a(href="https://orise.orau.gov/cer/rems/definitions.pdf", "Definitions")
+)
+
+# UI definition
+ui <- page_navbar(
+  title = "Radiation Exposure Monitoring System Dashboard",
+  nav_panel("Plots",
+            sidebarLayout(
+              sidebar_content,
+              mainPanel(
+                plotOutput("scatter"),
+                plotOutput("xHist"),
+                plotOutput("yHist"),
+                tags$a(href="https://www.energy.gov/ehss/occupational-radiation-exposure-rems-system-tools", "All data sourced from the Department of Energy REMS Query Tool")
+              )
+            )
+  ),
+  nav_panel("TED Segments",
+            sidebarLayout(
+              sidebar_content,
+              mainPanel(plotOutput("tedGraph"))
+            )
+  ),
+  nav_panel("Time Series",
+            sidebarLayout(
+              sidebar_content,
+              mainPanel(plotOutput("timeSeriesForecast"))
+            ),
   )
 )
 
+# Server logic
 server <- function(input, output, session) {
   subsetted <- reactive({
-    # Filter data based on user inputs
-    dat <- df %>% filter(Site %in% input$site &
-                        `Program Office` %in% input$program_office &
-                        `Monitoring Year` %in% seq(input$year[1], input$year[2]))
-    dat
+    df %>% filter(Site %in% input$site &
+                  `Program Office` %in% input$program_office &
+                  `Monitoring Year` %in% seq(input$year[1], input$year[2]))
+  })
+  
+  subsettedTS <- reactive({
+    df %>% filter(Site %in% input$site &
+                  `Program Office` %in% input$program_office &
+                  `Monitoring Year` %in% seq(input$year[1], input$year[2])) %>% as_tsibble(index = `Monitoring Year`,
+                      key=c("Program Office", "Operations Office", "Site", "Reporting Organization",
+                            "Facility Type", "Labor Category", "Occupation", "Monitoring Status")) %>% 
+      group_by(Site) %>% summarise(yvar = mean(!!input$yvar)) %>% fill_gaps()
   })
   
   output$scatter <- renderPlot({
+    p <- ggplot(subsetted(), aes_string(x = input$xvar, y = input$yvar)) + geom_point()
+    if (input$show_margins) {
+      p <- ggExtra::ggMarginal(p, type = "density", margins = "both", size = 8)
+    }
+
     # Plot selected data
     p <- ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) + geom_point()
     # Show marginal histograms if desired
@@ -102,22 +110,43 @@ server <- function(input, output, session) {
     p
   }, res = 100)
   
-  # Plot total effective dose (TODO: check dose def/original dashboard)
-  output$tedGraph <- renderPlot({
-    p <- subsetted() %>%
-      select(`Monitoring Year`, 
-             photon=`Collective ED Photon (person-mrem)`, 
-             neutron=`Collective ED Neutron (person-mrem)`, 
-             CED=`Collective CED (person-mrem)`) %>%
-      pivot_longer(c(photon, neutron, CED), 
-                   names_to="dose_type") %>%
-      ggplot(aes(x=`Monitoring Year`, fill=dose_type)) + geom_bar() + labs(x="Year", 
-                                                                           y="Collective Dose (Person-mrem)",
-                                                                           title="Components of TED")
+  # Create X Var and Y Var histograms
+  output$xHist <- renderPlot({
+    p <- ggplot(subsetted(), aes_string(x = input$xvar)) + geom_histogram()
+
     p
   }, res = 100)
   
+  output$yHist <- renderPlot({
+    p <- ggplot(subsetted(), aes_string(x = input$yvar)) + geom_histogram() + labs(x=input$yvar)
+    
+    p
+  }, res = 100)
+
+  
+  output$tedGraph <- renderPlot({
+    p <- subsetted() %>%
+      select(`Monitoring Year`,
+             photon=`Collective ED Photon (person-mrem)`, 
+             neutron=`Collective ED Neutron (person-mrem)`, 
+             CED=`Collective CED (person-mrem)`) %>%
+      pivot_longer(c(photon, neutron, CED), names_to="dose_type") %>%
+      ggplot(aes(x=`Monitoring Year`, fill=dose_type)) + geom_bar(position="stack") + labs(x="Year", y="Collective Dose (Person-mrem)", title="Components of TED")
+    p
+  }, res = 100)
+  
+  # Create TS forecast tab & plot with basic ARIMA model
+  output$timeSeriesForecast <- renderPlot({
+    p <- subsettedTS() %>%
+      model(ARIMA(yvar)) %>%
+      forecast(h = 10) %>%
+      autoplot(subsettedTS()) + labs(x = "Year",
+                                     y=input$yvar,
+                                     title="ARIMA Forecast")
+    
+    p
+  })
 }
 
-shinyApp(ui, server)
-
+# Run the application 
+shinyApp(ui = ui, server = server)
