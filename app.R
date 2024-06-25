dc675e770b0e6af3bfd0f74878e9b9acffb1cecf
Andrew Bowen <atbowen@gmail.com>
2024-06-24 21:51:48 -0400

    Add Time Series to main page (#8)
    * Add dose chart
    
    * clean up dashboard
    
    * add tidyr
    
    * revert file path to GH
    
    * Update TED title
    
    * Update README
    
    * adding tabbed layout
    
    * Add in basic TS
    
    * Add TS
    
    * add ARIMA forecast to TS tab
    
    * Add basic X and Y Histograms
    
    * Add time series functionality

--- app.R
+++ app.R
@@ -9,6 +9,7 @@ library(glue)
 library(tsibble)
 library(fabletools)
 library(fable)
+library(gridExtra)
 
 # Read in dataset
 df <- read.csv("https://raw.githubusercontent.com/andrewbowen19/rems-dashboard/main/rems-data.csv",
@@ -38,56 +39,45 @@ sidebar_content <- sidebarPanel(
 )
 
 # UI definition
-ui <- page_navbar(
-  title = "Radiation Exposure Monitoring System Dashboard",
-  nav_panel("Plots",
-            sidebarLayout(
-              sidebar_content,
-              mainPanel(
-                plotOutput("scatter"),
-                plotOutput("xHist"),
-                plotOutput("yHist"),
-                tags$a(href="https://www.energy.gov/ehss/occupational-radiation-exposure-rems-system-tools", "All data sourced from the Department of Energy REMS Query Tool")
-              )
-            )
-  ),
-  nav_panel("TED Segments",
-            sidebarLayout(
-              sidebar_content,
-              mainPanel(plotOutput("tedGraph"))
-            )
-  ),
-  nav_panel("Time Series",
-            sidebarLayout(
-              sidebar_content,
-              mainPanel(plotOutput("timeSeriesForecast"))
-            ),
+ui <- fluidPage(
+  titlePanel("Radiation Exposure Monitoring System Dashboard"),
+  sidebarLayout(
+    sidebar_content,
+    mainPanel(
+      plotOutput("scatter"),
+      plotOutput("timeSeriesForecast"),
+      plotOutput("barGraph"),
+      tags$a(href="https://www.energy.gov/ehss/occupational-radiation-exposure-rems-system-tools",
+             "All data sourced from the Department of Energy REMS Query Tool")
+    )
   )
 )
 
 # Server logic
 server <- function(input, output, session) {
+  
   subsetted <- reactive({
+    print(input$yvar)
     df %>% filter(Site %in% input$site &
                   `Program Office` %in% input$program_office &
                   `Monitoring Year` %in% seq(input$year[1], input$year[2]))
   })
   
   subsettedTS <- reactive({
+    print("bar")
+    print(as.symbol(input$yvar))
+    key_cols <- c("Program Office", "Operations Office", "Site", "Reporting Organization",
+                  "Facility Type", "Labor Category", "Occupation", "Monitoring Status")
     df %>% filter(Site %in% input$site &
                   `Program Office` %in% input$program_office &
-                  `Monitoring Year` %in% seq(input$year[1], input$year[2])) %>% as_tsibble(index = `Monitoring Year`,
-                      key=c("Program Office", "Operations Office", "Site", "Reporting Organization",
-                            "Facility Type", "Labor Category", "Occupation", "Monitoring Status")) %>% 
-      group_by(Site) %>% summarise(yvar = mean(!!input$yvar)) %>% fill_gaps()
+                  `Monitoring Year` %in% seq(input$year[1], input$year[2])) %>% 
+      as_tsibble(index = `Monitoring Year`, key=key_cols) %>% 
+      group_by(Site) %>% 
+      summarise(yValue = mean(!!as.symbol(input$yvar))) %>%
+      fill_gaps()
   })
   
   output$scatter <- renderPlot({
-    p <- ggplot(subsetted(), aes_string(x = input$xvar, y = input$yvar)) + geom_point()
-    if (input$show_margins) {
-      p <- ggExtra::ggMarginal(p, type = "density", margins = "both", size = 8)
-    }
-
     # Plot selected data
     p <- ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) + geom_point()
     # Show marginal histograms if desired
@@ -97,48 +87,38 @@ server <- function(input, output, session) {
     p
 
   }, res = 100)
-
-  # Plot time series
-  output$timeSeries <- renderPlot({
-    # Plot selected data summed over timeframe
-    p <- subsetted() %>% 
-      group_by(`Monitoring Year`) %>% 
-      mutate(Total = sum(!!input$yvar)) %>%
-      ggplot(aes(x=`Monitoring Year`, y=Total)) + geom_line() + labs(x="Year", 
-                                                                     y=glue("Total {input$yvar}"),
-                                                                     title=glue("Total {input$yvar} by Year"))
-    p
-  }, res = 100)
   
-  # Create X Var and Y Var histograms
-  output$xHist <- renderPlot({
-    p <- ggplot(subsetted(), aes_string(x = input$xvar)) + geom_histogram()
 
-    p
-  }, res = 100)
-  
-  output$yHist <- renderPlot({
-    p <- ggplot(subsetted(), aes_string(x = input$yvar)) + geom_histogram() + labs(x=input$yvar)
+
+  # Number of individuals with TED chart
+  output$barGraph <- renderPlot({
+    num_with_ted <- subsetted() %>% 
+      filter(`Monitoring Year` > 1988) %>%
+      group_by(`Monitoring Year`) %>%
+      summarise(`Number with TED` = sum(`Number with Meas. TED`))
     
-    p
-  }, res = 100)
-
+    ced_avg <- subsetted() %>%
+      group_by(`Monitoring Year`) %>%
+      summarise(`Collective CED` = mean(`Collective CED (person-mrem)`))
   
-  output$tedGraph <- renderPlot({
-    p <- subsetted() %>%
-      select(`Monitoring Year`,
-             photon=`Collective ED Photon (person-mrem)`, 
-             neutron=`Collective ED Neutron (person-mrem)`, 
-             CED=`Collective CED (person-mrem)`) %>%
-      pivot_longer(c(photon, neutron, CED), names_to="dose_type") %>%
-      ggplot(aes(x=`Monitoring Year`, fill=dose_type)) + geom_bar(position="stack") + labs(x="Year", y="Collective Dose (Person-mrem)", title="Components of TED")
-    p
+    
+    p1 <- num_with_ted %>% 
+      ggplot(aes(x=`Monitoring Year`, 
+                 y = `Number with TED`)) + geom_bar(stat='identity') + 
+      labs(x="Year", 
+           y ="Number of Individuals with Measured TED")
+    p2 <-ced_avg %>%  ggplot(aes(x=`Monitoring Year`, y=`Collective CED`)) + geom_bar(stat='identity') + 
+      labs(x="Year", 
+           y ="Collective CED")
+    grid.arrange(grobs=list(p1, p2), ncol=2)
   }, res = 100)
+  
+
   
   # Create TS forecast tab & plot with basic ARIMA model
   output$timeSeriesForecast <- renderPlot({
     p <- subsettedTS() %>%
-      model(ARIMA(yvar)) %>%
+      model(ARIMA(yValue)) %>%
       forecast(h = 10) %>%
       autoplot(subsettedTS()) + labs(x = "Year",
                                      y=input$yvar,
